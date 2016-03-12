5085f218d8964f2089a119f23ce5b28e3695da57
Alfredo Arias <alfredo@xiobit.com>
2016-03-11 21:29:20 -0400

    inicio de proyecto

---
+++ src/com/cursojava/petclinic/Main.java
@@ -0,0 +1,10 @@
+package com.cursojava.petclinic;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("*** PET CLINIC ***");
+
+    }
+}
---
+++ src/com/cursojava/petclinic/domain/Dummy.java
@@ -0,0 +1,26 @@
+package com.cursojava.petclinic.domain;
+
+/**
+ * Created by onix on 3/11/16.
+ */
+public class Dummy {
+
+    private String foo;
+    private String bar;
+
+    public String getFoo() {
+        return foo;
+    }
+
+    public void setFoo(String foo) {
+        this.foo = foo;
+    }
+
+    public String getBar() {
+        return bar;
+    }
+
+    public void setBar(String bar) {
+        this.bar = bar;
+    }
+}
