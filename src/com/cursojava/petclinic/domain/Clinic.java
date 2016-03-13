d643ee2fb840db9da640cf210746ffe8b79899c6
Hortencia <horte.floresb@gmail.com>
2016-03-13 19:16:55 -0400

    Setters y Getters creados para Pet y Clinic class

--- src/com/cursojava/petclinic/domain/Clinic.java
+++ src/com/cursojava/petclinic/domain/Clinic.java
@@ -4,4 +4,41 @@ package com.cursojava.petclinic.domain;
  * Created by onix on 3/11/16.
  */
 public class Clinic {
+
+    private String name;
+    private String nit;
+    private int numberRoom;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public int getNumberRoom() {
+        return numberRoom;
+    }
+
+    public void setNumberRoom(int numberRoom) {
+        this.numberRoom = numberRoom;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
--- src/com/cursojava/petclinic/domain/Pet.java
+++ src/com/cursojava/petclinic/domain/Pet.java
@@ -4,6 +4,40 @@ package com.cursojava.petclinic.domain;
  * Created by Hortencia on 11/03/2016.
  */
 public class Pet {
-    //private string namePet;
-    //private string
+    private String namePet;
+    private int age;
+    private String hairColor;
+    private String breed;
+
+    public String getNamePet() {
+        return namePet;
+    }
+
+    public void setNamePet(String namePet) {
+        this.namePet = namePet;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
 }
