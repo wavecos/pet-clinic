3ebb459ad106bffc0d9895d9d4ca5592ce48606a
sissi <carminia.eguivar@gmail.com>
2016-03-14 20:55:51 -0400

    clase veterinario

--- src/com/cursojava/petclinic/domain/Veterinarian.java
+++ src/com/cursojava/petclinic/domain/Veterinarian.java
@@ -4,4 +4,45 @@ package com.cursojava.petclinic.domain;
  * Created by academy on 3/11/16.
  */
 public class Veterinarian {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+
+    public Veterinarian(){
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+
 }
